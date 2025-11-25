8c2a36779eb6b7aef19de18dac8e4076940d60ec
Przemysław Świderski <przemekzgierz7@gmail.com>
2025-11-25 23:59:56 +0100

    Dependencies update (#28)

--- webpack.config.d/loaders.config.js
+++ webpack.config.d/loaders.config.js
@@ -1,25 +1,32 @@
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 config.plugins.push(new MiniCssExtractPlugin({
-        chunkFilename: "main.css",
+  chunkFilename: "main.css",
 }));
 
 config.module.rules.push(
-        {
-                resourceQuery: /raw/,
-                type: "asset/source",
-        },
-        {
-                test: /\.ya?ml$/,
-                use: "yaml-loader",
-        },
-        {
-                test: /\.(scss|sass)$/,
-                use: [
-                        MiniCssExtractPlugin.loader,
-                        "css-loader",
-                        "sass-loader",
-                ],
-                include: [require("path").resolve(__dirname, "kotlin/scss/styles.scss")],
-        },
+  {
+    resourceQuery: /raw/,
+    type: "asset/source",
+  },
+  {
+    test: /\.ya?ml$/,
+    use: "yaml-loader",
+  },
+  {
+    test: /\.(scss|sass)$/,
+    use: [
+      MiniCssExtractPlugin.loader,
+      "css-loader",
+      {
+        loader: 'sass-loader',
+        options: {
+          sassOptions: {
+            silenceDeprecations: ['mixed-decls', 'color-functions', 'global-builtin', 'import'],
+          }
+        }
+      }
+    ],
+    include: [require("path").resolve(__dirname, "kotlin/scss/styles.scss")],
+  },
 );
